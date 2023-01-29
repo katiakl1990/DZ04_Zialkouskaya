4639cdae8d225d62244dc63e34b5017fc0eea6f2
Katerina <121218479+katiakl1990@users.noreply.github.com>
2023-01-29 20:17:42 +0300

    DZ04

---
+++ DZ04_Zialkouskaya/Zadacha_29/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+string[] myArray(string step)
+{
+    string[] elements = new string [8];
+    elements = step.Split(",");
+    return elements;
+}
+
+void divide(string[] elements)
+{
+    Console.WriteLine(String.Join(", ", elements));  
+}
+
+void main()
+{
+    Console.WriteLine("Введите 8 чисел через запятую: ");
+    string num = Console.ReadLine()!;
+    divide(myArray(num));
+}
+
+main();
