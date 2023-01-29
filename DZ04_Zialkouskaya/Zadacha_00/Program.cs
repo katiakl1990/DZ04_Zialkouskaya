4fff61cee224228d9aeff24e931c10c3d1bc072b
Katerina <121218479+katiakl1990@users.noreply.github.com>
2023-01-29 20:20:47 +0300

    DZ04

---
+++ DZ04_Zialkouskaya/Zadacha_00/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача “со звездочкой”: 
+// Напишите функцию, которая принимает одно число - высоту елочки 
+// и рисует ее в консоли звездочками. (Пример см. презентацию)
+
+Console.Clear();
+
+Console.WriteLine("Введите высоту елочки: ");
+int height = int.Parse(Console.ReadLine()!);
+
+for (int i = height; i >= 0; i--)
+    {
+        
+        for (int j = height - i; j < height; j++)
+        {
+            Console.Write(" ");
+        }
+        if (i == height)
+        {
+            Console.WriteLine("*");
+            continue;
+        }
+        for (int j = 0; j < height - i; j++)
+        {
+            Console.Write("*");
+        }
+        Console.Write("*");
+        for (int j = 0; j < height - i; j++)
+        {
+            Console.Write("*");
+        }
+            Console.WriteLine();
+    }
+    for (int i = height; i > 0; i--)
+    {
+        Console.Write(" ");
+    } 
+Console.ReadKey(); 
