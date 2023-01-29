1b8798ea64835337d0b46b5a34a410a365720bbc
Katerina <121218479+katiakl1990@users.noreply.github.com>
2023-01-29 20:12:17 +0300

    DZ04

---
+++ DZ04_Zialkouskaya/Zadacha_27/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 27: Напишите программу, которая принимает 
+// на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+void sum_num(int number)
+{   
+    int result = 0;
+    int initial_number = number;    
+    while (number > 0)              
+    {
+        result += number % 10;
+        number /= 10; 
+    }
+    Console.WriteLine($"Сумма цифр в числе {initial_number} = {result}");
+}
+
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine()!);
+sum_num(number);
