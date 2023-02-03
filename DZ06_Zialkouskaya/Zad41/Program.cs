40c91e0ae1cfd89004b4c48223cef5b5959f8d17
Katerina <121218479+katiakl1990@users.noreply.github.com>
2023-02-04 00:47:16 +0300

    DZ06

---
+++ DZ06_Zialkouskaya/Zad41/Program.cs
@@ -0,0 +1,59 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+int[] String_to_array(string numbers)
+{
+    numbers = numbers + " ";  
+    bool flag = false;
+    int count = 0;
+    for (int i = 0; i < numbers.Length; i++)                         
+    {
+        if (Char.IsNumber(numbers[i])) flag = true;                  
+        else
+        {
+            if (flag) count++;                                        
+            flag = false;
+        }
+    }
+
+    string work_string = string.Empty;
+    int[] array_of_numbers = new int[count];
+    count = 0;
+    for (int i = 0; i < numbers.Length; i++) 
+    {
+        if (Char.IsNumber(numbers[i]) || (numbers[i] == '-')) work_string += numbers[i];
+        else
+        {       
+            if (work_string != string.Empty)
+            {
+                array_of_numbers[count] = int.Parse(work_string);
+                count++;
+                work_string = string.Empty;
+            }
+        }
+    }
+
+    return array_of_numbers;
+}
+
+int count_el_greater_zero(int[] array)
+{
+    int counter = 0;
+    foreach (int el in array)
+    {
+        if (el > 0) counter++;
+    }
+    return counter;
+}
+
+Console.Write("Введите числа через разделитель: ");
+string user_string = Console.ReadLine();
+
+int[] array_from_user_string = String_to_array(user_string);
+int gretater_than_zero = count_el_greater_zero(array_from_user_string);
+
+Console.Write("Даны числа: ");
+Console.WriteLine(String.Join("  ", array_from_user_string));
+Console.WriteLine($"Из них {gretater_than_zero} больше 0");
---
+++ DZ06_Zialkouskaya/Zad43/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+double[] IntersectionPoint(double k1, double b1, double k2, double b2)
+{
+    double[] valuesArray = new double[2];
+
+        valuesArray[0] = (b2 - b1) / (k1 - k2);
+        valuesArray[1] = k1 * (b2 - b1) / (k1 - k2) + b1;
+    
+    return valuesArray;
+
+}
+
+void main()
+{
+    Console.Clear();
+    Console.Write("Введите k1: ");
+    var k1 = Convert.ToDouble(Console.ReadLine());
+    Console.Write("Введите b1: ");
+    var b1 = Convert.ToDouble(Console.ReadLine());
+    Console.Write("Введите k2: ");
+    var k2 = Convert.ToDouble(Console.ReadLine());
+    Console.Write("Введите b2: ");
+    var b2 = Convert.ToDouble(Console.ReadLine());
+
+    if ((k1 == k2) && (b1 == b2))
+        Console.WriteLine("Прямые совпадают");
+
+    else if (k1 == k2)
+        Console.WriteLine("Прямые параллельны");
+
+    else if (k1 != k2)
+    {
+        double[] pointCross = IntersectionPoint(k1, b1, k2, b2);
+        Console.WriteLine($"Прямые пересекаются в точке {pointCross[0]};{pointCross[1]}");
+    }
+}
+main();
